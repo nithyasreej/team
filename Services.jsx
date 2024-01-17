56344aef6a3f6348451d50f23d52385cebe9d38d
J Nithyasree <95671947+nithyasreej@users.noreply.github.com>
2024-01-17 19:31:49 +0530

    Add files via upload

---
+++ Services.jsx
@@ -0,0 +1,88 @@
+
+import React from "react";
+
+function Services () {
+    let message = `There are many variations of passages of Lorem Ipsum available but the \n majority have suffered alteration in some injected humour.`;
+    return (
+      <section class="section-white">
+ 
+    <div class="container">
+ 
+    <h2>"Meet The Team"</h2>
+<br/>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="https://imgs.search.brave.com/yqSfK7NoVl9WBQhUQMcljmTAWfuGd0rKyZqb0ImrdAE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91eHdp/bmcuY29tL3dwLWNv/bnRlbnQvdGhlbWVz/L3V4d2luZy9kb3du/bG9hZC9idXNpbmVz/cy1wcm9mZXNzaW9u/YWwtc2VydmljZXMv/YnV0bGVyLWljb24u/c3Zn.svg" alt="Jane" style={{ width: '10%' }}/>
+      <div class="container">
+        <h2>lorem ipsum </h2>
+        <p class="title">lorem ipsum </p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="https://imgs.search.brave.com/yqSfK7NoVl9WBQhUQMcljmTAWfuGd0rKyZqb0ImrdAE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91eHdp/bmcuY29tL3dwLWNv/bnRlbnQvdGhlbWVz/L3V4d2luZy9kb3du/bG9hZC9idXNpbmVz/cy1wcm9mZXNzaW9u/YWwtc2VydmljZXMv/YnV0bGVyLWljb24u/c3Zn.svg" alt="Jane" style={{ width: '10%' }}/>
+      <div class="container">
+        <h2>lorem ipsum </h2>
+        <p class="title">lorem ipsum </p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="https://imgs.search.brave.com/yqSfK7NoVl9WBQhUQMcljmTAWfuGd0rKyZqb0ImrdAE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91eHdp/bmcuY29tL3dwLWNv/bnRlbnQvdGhlbWVz/L3V4d2luZy9kb3du/bG9hZC9idXNpbmVz/cy1wcm9mZXNzaW9u/YWwtc2VydmljZXMv/YnV0bGVyLWljb24u/c3Zn.svg" alt="Jane" style={{ width: '10%' }}/>
+      <div class="container">
+        <h2>lorem ipsum </h2>
+        <p class="title">lorem ipsum </p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="https://imgs.search.brave.com/yqSfK7NoVl9WBQhUQMcljmTAWfuGd0rKyZqb0ImrdAE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91eHdp/bmcuY29tL3dwLWNv/bnRlbnQvdGhlbWVz/L3V4d2luZy9kb3du/bG9hZC9idXNpbmVz/cy1wcm9mZXNzaW9u/YWwtc2VydmljZXMv/YnV0bGVyLWljb24u/c3Zn.svg" alt="Jane" style={{ width: '10%' }}/>
+      <div class="container">
+        <h2>lorem ipsum </h2>
+        <p class="title">lorem ipsum </p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="https://imgs.search.brave.com/yqSfK7NoVl9WBQhUQMcljmTAWfuGd0rKyZqb0ImrdAE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91eHdp/bmcuY29tL3dwLWNv/bnRlbnQvdGhlbWVz/L3V4d2luZy9kb3du/bG9hZC9idXNpbmVz/cy1wcm9mZXNzaW9u/YWwtc2VydmljZXMv/YnV0bGVyLWljb24u/c3Zn.svg" alt="Jane" style={{ width: '10%' }}/>
+      <div class="container">
+        <h2>lorem ipsum </h2>
+        <p class="title">lorem ipsum </p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="https://imgs.search.brave.com/yqSfK7NoVl9WBQhUQMcljmTAWfuGd0rKyZqb0ImrdAE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91eHdp/bmcuY29tL3dwLWNv/bnRlbnQvdGhlbWVz/L3V4d2luZy9kb3du/bG9hZC9idXNpbmVz/cy1wcm9mZXNzaW9u/YWwtc2VydmljZXMv/YnV0bGVyLWljb24u/c3Zn.svg" alt="Jane" style={{ width: '10%' }}/>
+      <div class="container">
+        <h2>lorem ipsum </h2>
+        <p class="title">lorem ipsum </p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+      </div>
+    </div>
+  </div>
+  
+</div>
+    </div> 
+
+    </section>
+    )
+}
+
+export default Services;
